461cfcbdcff548f0730c5f668a811b27651b6c20
ashok34kumar <163623192+ashok34kumar@users.noreply.github.com>
2025-02-03 12:15:46 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+import Counter from "./Counter";
+const App = () => {
+  return (
+    <div>
+      <h1>Welcome to My Parcel Project</h1>
+      <Counter />
+    </div>
+  );
+};
+
+export default App;
